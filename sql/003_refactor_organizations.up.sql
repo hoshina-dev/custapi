e44389fc12af0e41e8f083c7b3962f26e521c91b
Chantouch Narasetaphan <inecrft4747@gmail.com>
2026-01-12 16:12:41 +0700

    Add further sql script for users table

--- sql/003_refactor_organizations.up.sql
+++ sql/003_refactor_organizations.up.sql
@@ -2,6 +2,6 @@ ALTER TABLE organizations
 ADD COLUMN geom GEOMETRY(POINT, 4326) NOT NULL,
 ADD COLUMN address TEXT,
 ADD COLUMN description TEXT,
-ADD COLUMN image_urls TEXT[];
+ADD COLUMN image_urls TEXT[] NOT NULL DEFAULT '{}';
 
 CREATE INDEX IF NOT EXISTS organizations_geom_idx ON organizations USING GIST (geom);
