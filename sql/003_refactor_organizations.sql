bb49ab3a4efea9c3e3b68996365504e423991c3a
Chantouch Narasetaphan <inecrft4747@gmail.com>
2026-01-12 15:18:42 +0700

    Add index to geom column

--- sql/003_refactor_organizations.sql
+++ sql/003_refactor_organizations.sql
@@ -3,3 +3,5 @@ ADD COLUMN geom GEOMETRY(POINT, 4326) NOT NULL,
 ADD COLUMN address TEXT,
 ADD COLUMN description TEXT,
 ADD COLUMN image_urls TEXT[];
+
+CREATE INDEX IF NOT EXIST organizations_geom_idx ON organizations USING GIST (geom);
