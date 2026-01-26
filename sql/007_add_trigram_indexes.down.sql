ccba96a18490bcdfd9e851f0761b143db9fbf73d
WasinUddy <wsilakong@gmail.com>
2026-01-26 13:20:22 +0700

    Create trigram index

--- sql/007_add_trigram_indexes.down.sql
+++ sql/007_add_trigram_indexes.down.sql
@@ -1,6 +1,9 @@
 -- Migration: 007_add_trigram_indexes
 -- Description: Rollback trigram indexes on users and organizations name columns
 
+-- Drop trigram index on users.email
+DROP INDEX IF EXISTS idx_users_email_trgm;
+
 -- Drop trigram index on users.name
 DROP INDEX IF EXISTS idx_users_name_trgm;
 
